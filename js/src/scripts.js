eb2d9c744baf9dc1ec01f085e6998f58ffa3b713
Aled Carver <Aled Carver>
2017-04-02 16:40:00 -0700

    MASTER: Added responsive and playlist capability

--- js/src/scripts.js
+++ js/src/scripts.js
@@ -1,30 +1,75 @@
 $(document).ready(function () {
-    var tune = new Audio('assets/audio/02_Three_Little_Birds.mp3','assets/audio/02_Three_Little_Birds.ogg');
+    
+    var tuneSrcArray = [];
+    var tuneTypeArray = [];
+    var playerIndex = 0;
+    
+    var init = function () {
+        $('.tune.active source').each(function (index) {
+            var tuneSrc = $(this).attr('src');
+            var tuneType = $(this).attr('type');
+
+            tuneSrcArray[index] = tuneSrc;
+            tuneTypeArray[index]= tuneType;
+        });
+
+        tune = new Audio(tuneSrcArray);
+
+        $.each(tuneTypeArray, function (index) {
+            if (tune.canPlayType(tuneTypeArray[index]+';')) {
+                tune.type= tuneTypeArray[index];
+                tune.src= tuneSrcArray[index];
+                return false;
+            }
+        });
+        
+        return tune;
+    };
+    
+    var tune = init();
+    
+    $('.player-playlist ul li').on('click', function () {
+        $('.player-playlist ul li').removeClass('active');
+        pause.trigger('click');
+        
+        $(this).addClass('active');
+        
+        $('img.active').attr('src', $(this).data('img')); 
+        $('h1.title').html($(this).data('title')); 
+        $('h2.album').html($(this).data('album')); 
+        $('h2.artist').html($(this).data('artist')); 
+        
+        var assets = $(this).data('asset');
+        assets = assets.split(',');
+        var types = $(this).data('type');
+        types = types.split(',');
+        $('.player-audio.tune.active source').remove();
+       
+        $.each(assets, function (i) {
+            $('.player-audio.tune.active').append('<source src="'+assets[i]+'" type="'+types[i]+'">');
+        });
+        
+        var tune = init();
+        
+        tuneEvents();
+    });
     
     var play = $('.play');
     var pause = $('.pause');
     var rewind = $('.rewind');
     var fastforward = $('.fastforward');
     var interval;
-    
-    if (tune.canPlayType('audio/mpeg;')) {
-  	     tune.type= 'audio/mpeg';
-  	     tune.src= 'assets/audio/02_Three_Little_Birds.mp3';
-	} else {
-  	     tune.type= 'audio/ogg';
-  	     tune.src= 'assets/audio/02_Three_Little_Birds.ogg';
-	}
-    
+    var next = $('.next');
+    var back = $('.back');
+   
     play.on('click', function(e) {
 		tune.play();
-
 		$(this).hide();
         pause.fadeIn();
 	});
     
     pause.on('click', function(e) {
 		tune.pause();
-        
         $(this).hide();
         play.fadeIn();
 	});
@@ -50,14 +95,43 @@ $(document).ready(function () {
         tune.playbackRate = 1.0;
     });
     
-    tune.addEventListener("timeupdate", function() {
-        var currentTime = tune.currentTime;
-        var duration = tune.duration;
-        $('.player-progress').stop(true,true).animate({'width':(currentTime+0.25)/duration*100+'%'},250,'linear');
-        
-        var mins = "0" + Math.floor(currentTime / 60);
-        var secs = "0" +  Math.floor(currentTime - mins * 60);
-        var progress = mins.substr(-1) + ":" + secs.substr(-2);
-        $('.player-start').html(progress);
+    var tuneEvents = function () {
+        tune.addEventListener('timeupdate', function() {
+            $('.player-end').html('0:00');
+            
+            var currentTime = tune.currentTime;
+            var duration = tune.duration;
+            $('.player-progress').stop(true,true).animate({'width':(currentTime+0.25)/duration*100+'%'},250,'linear');
+
+            var mins = "0" + Math.floor(currentTime / 60);
+            var secs = "0" +  Math.floor(currentTime - mins * 60);
+            var progress = mins.substr(-1) + ":" + secs.substr(-2);
+            $('.player-start').html(progress);
+            
+            if (tune.duration) { 
+                var minsx = "0" + Math.floor(duration / 60);
+                var secsx = "0" +  Math.floor(duration - mins * 60);
+                var durationFormat = minsx.substr(-1) + ":" + secsx.substr(-2);
+                $('.player-end').html(durationFormat);
+            }
+        });
+    };
+    
+    next.on('click', function () {
+        if (playerIndex === ( $(".player-playlist ul li").size() - 1 ) ) {
+            playerIndex = 0;
+        } else {
+            playerIndex++;
+        }
+        console.log(playerIndex);
+        $(".player-playlist ul li[data-index='" + playerIndex + "']").click();
+    });
+    back.on('click', function () {
+       if (playerIndex === 0) {
+            playerIndex = ( $(".player-playlist ul li").size() - 1);
+        } else {
+            playerIndex--;
+        }
+        $(".player-playlist ul li[data-index='" + playerIndex + "']").click();
     });
 });
